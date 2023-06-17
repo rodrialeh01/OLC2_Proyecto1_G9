04840f44606f7e1ba885084959a9ae9d4d43d55b
rodrialeh01 <rodrialehdl@gmail.com>
2023-06-16 19:28:05 -0600

    Se puede descargar el AST en .svg

--- Frontend/src/app/components/ast/ast.component.ts
+++ Frontend/src/app/components/ast/ast.component.ts
@@ -2,12 +2,11 @@ import { Component, ElementRef, HostBinding, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { graphviz } from 'd3-graphviz';
 
-import { jsPDF } from 'jspdf';
 import { Archivo } from 'src/app/models/Archivo';
 import { UserService } from 'src/app/services/user.service';
-// @ts-ignore
 import Viz from 'viz.js';
 import 'viz.js/full.render.js';
+import { Module, render } from 'viz.js/full.render.js';
 import 'viz.js/lite.render.js';
 
 @Component({
@@ -45,36 +44,21 @@ export class AstComponent implements OnInit{
 
   }
 
-  generatePDF(): void{
-  const viz = new Viz({ worker: 'true' });
-
-  viz.renderString(this.textoG, { format: 'svg' })
-    .then((result: string) => { 
-      const pdf = new jsPDF();
-      const width = pdf.internal.pageSize.getWidth();
-      const height = pdf.internal.pageSize.getHeight();
-  
-      const svgUrl = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(result)}`;
-      const svgImage = new Image();
-  
-      svgImage.onload = () => {
-        const canvas = document.createElement('canvas');
-        const context = canvas.getContext('2d');
-        canvas.width = width;
-        canvas.height = height;
-  
-        context?.drawImage(svgImage, 0, 0, width, height);
-        const svgData = canvas.toDataURL('image/svg+xml');
-  
-        pdf.addImage(svgData, 'SVG', 0, 0, width, height);
-        pdf.save('graphviz.pdf');
-      };
-  
-      svgImage.src = svgUrl;
-    })
-    .catch((error:any) => {
-      console.error(error);
-    });
+  generateSVG(): void{
+    const viz = new Viz({ Module, render });
+    viz.renderString(this.textoG, { format: 'svg' })
+      .then((svg: string) => {
+        const blob = new Blob([svg], { type: 'image/svg+xml' });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'AST.svg';
+        link.click();
+        URL.revokeObjectURL(url);
+      })
+      .catch((error: any) => {
+        console.error(error);
+      });
   }
 
   graph(){
